0cb842038384a15af8ebd16928c23a6d9dd62f5a
philogicae <38438271+philogicae@users.noreply.github.com>
2025-08-11 20:03:31 +0300

    feat: add database extensions for job queues, async HTTP, scheduling and hstore

--- init_volumes/db/custom.sql
+++ init_volumes/db/custom.sql
@@ -1,4 +1,15 @@
 BEGIN;
-  -- Add pgvector extension
-  CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA extensions;
+-- For vector operations
+CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA extensions;
+-- For queueing and processing jobs
+-- (pgmq will create its own schema)
+CREATE EXTENSION IF NOT EXISTS pgmq;
+-- For async HTTP requests
+CREATE EXTENSION IF NOT EXISTS pg_net WITH SCHEMA extensions;
+-- For scheduled processing and retries
+-- (pg_cron will create its own schema)
+CREATE EXTENSION IF NOT EXISTS pg_cron;
+-- For clearing embeddings during updates
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA extensions;
+
 COMMIT;
